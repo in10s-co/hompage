8e753e000cd8d85e3b55fe2df42cb4211dcf0d57
hyungi <hkgo6040@gmail.com>
2023-09-07 17:12:09 +0900

    main, css

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,13 @@
-document.write('<script async src="https://tally.so/widgets/embed.js"></script>');
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/mobile-detect/1.4.5/mobile-detect.min.js"></script>');
+document.write(
+  '<script async src="https://tally.so/widgets/embed.js"></script>'
+);
+document.write(
+  '<script src="https://cdnjs.cloudflare.com/ajax/libs/mobile-detect/1.4.5/mobile-detect.min.js"></script>'
+);
 
-window.onload = function () {
+window.addEventListener("load", function () {
+    // alert(window.location.host);
+  if (window.location.host == "in10s.co") {
     const toggleBtn = document.querySelector(".buttonIcon");
     const header = document.querySelector(".header");
 
@@ -10,46 +16,64 @@ window.onload = function () {
       toggleBtn.classList.toggle("buttonIconNav");
       toggleBtn.classList.toggle("buttonIconClose");
     });
-  };
-
-  function openTally(tag, mode, link) {
-    const url_pathname = window.location.pathname + "";
-    var device = new MobileDetect(navigator.userAgent);
-    const pcOs = 'win16|win32|win64|windows|mac|macintel|linux|freebsd|openbsd|sunos'.indexOf(navigator.platform.toLowerCase()) >= 0;
-
-    Tally.openPopup("3NlXpN", {
-      layout: "modal",
-      width: 400,
-      hideTitle: false,
-      hiddenFields: {
-          from: url_pathname,
-          tag: "{tag: " + tag + ",\n" +"mobile: " + device.mobile() + ",\nphone: " + device.phone() + ",\ntablet: " + device.tablet() + ",\nuserAgent: " + device.userAgent() + ",\nmobileOs: " + device.os() + ",\npcOs: " + pcOs +"\n}",
-      },
-      onOpen: () => {
-        // The popup was opened, mark the form as seen
-        const tally_popup = document.querySelector(".tally-popup iframe");
-        if (link && mode == "page") {
-          tally_popup.src = "https://www.in10s.co/service/acceptancerate/" + link;
-          setTimeout(() => {
-              var popup_display = setInterval(() => {
-              if (tally_popup.readyState == "complete") {
-                tally_popup.style.display = 'flex';
-                clearInterval(popup_display);
-              }
-            }, 100);
-          }, 300);
-        }else {tally_popup.style.display = 'flex';}
-      },
-      onClose: () => {
-        // The popup was closed
-        // document.querySelector(".cta").style.display = 'flex';
-      },
-      onSubmit: (payload) => {
-        // Form was submitted, use the answers payload in your application
-        //window.open("https://bit.ly/purchase_15000", "", "_blank");
-        if (link && mode == "pay") {
-            window.open("https://qr.kakaopay.com/" + link, "", "_blank");
-        }
-      },
-    });
   }
+});
+
+function openTally(tag, mode, link) {
+  const url_pathname = window.location.pathname + "";
+  const device = new MobileDetect(navigator.userAgent);
+
+  Tally.openPopup("3NlXpN", {
+    layout: "modal",
+    width: 400,
+    hideTitle: false,
+    hiddenFields: {
+      from: url_pathname,
+      tag:
+        "{tag: " +
+        tag +
+        "," +
+        "mobile: " +
+        device.mobile() +
+        ",phone: " +
+        device.phone() +
+        ",tablet: " +
+        device.tablet() +
+        ",userAgent: " +
+        device.userAgent() +
+        ",mobileOs: " +
+        device.os() +
+        ",pcOs: " +
+        navigator.platform +
+        "}"
+    },
+    onOpen: () => {
+      // The popup was opened, mark the form as seen
+      const tally_popup = document.querySelector(".tally-popup iframe");
+      if (link && mode == "page") {
+        tally_popup.src = "https://www.in10s.co/service/acceptancerate/" + link;
+        setTimeout(() => {
+          var popup_display = setInterval(() => {
+            if (tally_popup.readyState == "complete") {
+              tally_popup.style.display = "flex";
+              clearInterval(popup_display);
+            }
+          }, 100);
+        }, 300);
+      } else {
+        tally_popup.style.display = "flex";
+      }
+    },
+    onClose: () => {
+      // The popup was closed
+      // document.querySelector(".cta").style.display = 'flex';
+    },
+    onSubmit: (payload) => {
+      // Form was submitted, use the answers payload in your application
+      //window.open("https://bit.ly/purchase_15000", "", "_blank");
+      if (link && mode == "pay") {
+        window.open("https://qr.kakaopay.com/" + link, "", "_blank");
+      }
+    },
+  });
+}
